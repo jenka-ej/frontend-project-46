8e81ef44eb46442a600e9f8f1b7ff625b5d3f2b5
Evgeniy <Zhekafilipp@mail.ru>
2022-09-05 14:50:15 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import fs from 'fs';
 import path from 'path';
 
-const readFilePath = (filepath) = path.resolve(process.cwd(), filepath);
-const readFile = (filepath) = fs.readFileSync(readFilePath(filepath), 'utf-8');
-const getFormat = (filename) = filename.split('.')[1];
+const readFilePath = (filepath) => path.resolve(process.cwd(), filepath);
+const readFile = (filepath) => fs.readFileSync(readFilePath(filepath), 'utf-8');
+const getFormat = (filename) => filename.split('.')[1];
 
 const genDiff = (file1, file2) => {
 
 };
+
 export default genDiff;
