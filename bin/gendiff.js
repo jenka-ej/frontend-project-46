f94357792af207155302c98a265f316b3bf797a0
Evgeniy <Zhekafilipp@mail.ru>
2022-09-05 11:23:39 +0300

    .

---
+++ bin/gendiff.js
@@ -0,0 +1,13 @@
+#!usr/bin/env node
+
+import { Command } from 'commander/esm.mjs';
+
+const program = new Command();
+
+program
+  .version('0.1.0')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .option('--f, --format <type>', 'output format');
+
+program.parse(process.argv);
