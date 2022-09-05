86dfa88ef17838bd12c257df4c36333cef047289
Evgeniy <Zhekafilipp@mail.ru>
2022-09-05 16:56:25 +0300

    upd#21

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -11,7 +11,7 @@ program
   .argument('<filepath1>')
   .argument('<filepath2>')
   .option('-f, --format <type>', 'output format');
-  .action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2));
+  .action((filepath1, filepath2, options) => {
+    console.log(genDiff(filepath1, filepath2, options.format));
   })
   .parse(process.argv);
