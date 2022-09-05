66e22a986e1c5886b7b92dd71d4e76a354983104
Evgeniy <Zhekafilipp@mail.ru>
2022-09-05 16:49:22 +0300

    upd

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,13 +1,17 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander/esm.mjs';
-
-const program = new Command();
+import genDiff from '../src/index.js;
 
 program
   .version('0.1.0')
-  .description('Compares two configuration files and shows a difference.')
-  .argument('<filepath1> <filepath2>')
-  .option('-f, --format <type>', 'output format');
+  .description('Compares two configuration files and shows a difference.');
 
-program.parse(process.argv);
+program
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format <type>', 'output format');
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  })
+  .parse(process.argv);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import fs from 'fs';
 import path from 'path';
+import compareData from './compareData.js';
 
-const readFilePath = (filepath) => path.resolve(process.cwd(), filepath);
-const readFile = (filepath) => fs.readFileSync(readFilePath(filepath), 'utf-8');
-const getFormat = (filename) => filename.split('.')[1];
+const getExtension = (filepath) => path.extname(filepath);
+const getAbsolutePath = (filepath) => path.resolve(filepath);
+const readFileSync = (filepath) => fs.readFileSync(filepath, 'utf-8');
 
-const genDiff = (file1, file2) => {
+const genDiff = (fpath1, fpath2) => {
+  const ext1 = getExtension(fpath1);
+  const ext2 = getExtension(fpath2);
 
+  const parsedFile1 = parse(readFileSync(getAbsolutePath(fpath1)), ext1);
+  const parsedFile2 = parse(readFileSync(getAbsolutePath(fpath2)), ext2);
+
+  return compareData(parsedFile1, parsedFile2);
 };
 
 export default genDiff;
