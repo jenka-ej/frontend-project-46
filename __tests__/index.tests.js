cf7ccb5410befe595a5b626b1083f2387abf706a
Evgeniy <Zhekafilipp@mail.ru>
2022-09-16 14:51:14 +0300

    test fix

--- __tests__/index.tests.js
+++ __tests__/index.tests.js
@@ -1,9 +1,9 @@
 import fs from 'fs';
 import path, { dirname } from 'path';
-import { fileURLtoPath } from 'url';
+import { fileURLToPath } from 'node:url';
 import genDiff from '../src/index.js';
 
-const __filename = fileURLtoPath(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
@@ -12,6 +12,6 @@ const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8'
 test('test to compare the actual and desired results', () => {
   const expected = readFile('test1.txt');
   const actual = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
-  expect(actual).toBe(expected);
+  expect(actual).toEqual(expected);
 });
 	
