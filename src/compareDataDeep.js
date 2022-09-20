1e21363aa66868957d5df888ae1ee3b5a031d622
Evgeny Filippov <85373955+jenka-ej@users.noreply.github.com>
2022-09-20 12:00:36 +0300

    Update compareDataDeep.js

--- src/compareDataDeep.js
+++ src/compareDataDeep.js
@@ -5,7 +5,7 @@ const diff = (obj1, obj2) => {
   const childrenObj2 = getChildren(obj2);
   const childrenMass = childrenObj1.concat(childrenObj2);
   const resultMass = childrenMass.filter((child, index) =>  {
-    childrenMass.indexOf(child) === index);
+    childrenMass.indexOf(child) === index;
   }).sort();
 
   const cb = (acc, key) => {
