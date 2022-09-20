71d41acb314741e245e8b89e553fa3baf455cbf6
Evgeny Filippov <85373955+jenka-ej@users.noreply.github.com>
2022-09-20 16:52:40 +0300

    Update stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,6 +1,6 @@
 const goodView = (obj) => {
   const objToStr = JSON.stringify(obj, null, 4);
-  const goodResult = objToStr.split('"').join('').split(',').join('')
+  const goodResult = objToStr.split("'").join('').split(',').join('')
     .split('  + ')
     .join('+ ')
     .split('  - ')
