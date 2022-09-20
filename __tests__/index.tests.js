3b4cb37fcfcd1a310412b897573c41ff809af483
Evgeny Filippov <85373955+jenka-ej@users.noreply.github.com>
2022-09-20 15:19:12 +0300

    Update index.tests.js

--- __tests__/index.tests.js
+++ __tests__/index.tests.js
@@ -1,21 +1,21 @@
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'node:url';
+import fs from 'fs';
 import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const expected = `{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}`;
+const readFile = (file) => {
+  const filePath = getFixturePath(file);
+  const content = fs.readFileSync(filePath, 'utf-8');
+  return content;
+};
+
+const expectedStylish = readFile('resultStylish.txt');
 
 test('test to compare the actual and desired results', () => {
   const actual = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
-  expect(actual).toEqual(expected);
+  expect(actual).toEqual(expectedStylish);
 });
