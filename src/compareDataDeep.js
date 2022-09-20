25955502540ae60ec14fccebc10882674a64c422
Evgeniy <Zhekafilipp@mail.ru>
2022-09-20 11:54:47 +0300

    little fix

--- src/compareDataDeep.js
+++ src/compareDataDeep.js
@@ -1,54 +1,49 @@
 const diff = (obj1, obj2) => {
+  const getChildren = (obj) => Object.keys(obj);
 
-    const getChildren = (obj) => {
-        return Object.keys(obj);
-    };
+  const childrenObj1 = getChildren(obj1);
+  const childrenObj2 = getChildren(obj2);
+  const childrenMass = childrenObj1.concat(childrenObj2);
+  const resultMass = childrenMass.filter((child, index) => childrenMass.indexOf(child) === index).sort();
 
-    const childrenObj1 = getChildren(obj1);
-    const childrenObj2 = getChildren(obj2);
-    const childrenMass = childrenObj1.concat(childrenObj2);
-    const resultMass = childrenMass.filter((child, index) => childrenMass.indexOf(child) === index).sort();
-
-    const cb = (acc, key) => {
-        const obj1HasKey = obj1.hasOwnProperty(key);
-        const obj2HasKey = obj2.hasOwnProperty(key);
-        const value1 = obj1[key];
-        const value2 = obj2[key];
-        const obj1KeyIsObject = (typeof value1 === 'object' && value1 !== null && !Array.isArray(value1));
-        const obj2KeyIsObject = (typeof value2 === 'object' && value2 !== null && !Array.isArray(value2));
-        if (obj1HasKey && obj2HasKey && obj1KeyIsObject && obj2KeyIsObject) {
-            acc[`${key}`] = diff(value1, value2);
-            return acc;
-        } else if (obj1HasKey && obj2HasKey && obj1KeyIsObject && !obj2KeyIsObject) {
-            acc[`- ${key}`] = value1;
-            acc[`+ ${key}`] = value2;
-            return acc;
-        } else if (obj1HasKey && obj2HasKey && !obj1KeyIsObject && obj2KeyIsObject) {
-            acc[`- ${key}`] = value1;
-            acc[`+ ${key}`] = value2;
-            return acc;
-        } else if (obj1HasKey && !obj2HasKey && obj1KeyIsObject) {
-            acc[`- ${key}`] = value1;
-            return acc;
-        } else if (!obj1HasKey && obj2HasKey && obj2KeyIsObject) {
-            acc[`+ ${key}`] = value2;
-            return acc;
-        } else if (obj1HasKey && obj2HasKey) {
-            if (value1 === value2) {
-                acc[`${key}`] = value1;
-                return acc;
-            } else {
-                acc[`- ${key}`] = value1;
-                acc[`+ ${key}`] = value2;
-                return acc;
-            }
-        } else if (obj1HasKey && !obj2HasKey) {
-            acc[`- ${key}`] = value1;
-            return acc;
-        } else {
-            acc[`+ ${key}`] = value2;
-            return acc;
-        }
+  const cb = (acc, key) => {
+    const obj1HasKey = obj1.hasOwnProperty(key);
+    const obj2HasKey = obj2.hasOwnProperty(key);
+    const value1 = obj1[key];
+    const value2 = obj2[key];
+    const obj1KeyIsObject = (typeof value1 === 'object' && value1 !== null && !Array.isArray(value1));
+    const obj2KeyIsObject = (typeof value2 === 'object' && value2 !== null && !Array.isArray(value2));
+    if (obj1HasKey && obj2HasKey && obj1KeyIsObject && obj2KeyIsObject) {
+      acc[`${key}`] = diff(value1, value2);
+      return acc;
+    } if (obj1HasKey && obj2HasKey && obj1KeyIsObject && !obj2KeyIsObject) {
+      acc[`- ${key}`] = value1;
+      acc[`+ ${key}`] = value2;
+      return acc;
+    } if (obj1HasKey && obj2HasKey && !obj1KeyIsObject && obj2KeyIsObject) {
+      acc[`- ${key}`] = value1;
+      acc[`+ ${key}`] = value2;
+      return acc;
+    } if (obj1HasKey && !obj2HasKey && obj1KeyIsObject) {
+      acc[`- ${key}`] = value1;
+      return acc;
+    } if (!obj1HasKey && obj2HasKey && obj2KeyIsObject) {
+      acc[`+ ${key}`] = value2;
+      return acc;
+    } if (obj1HasKey && obj2HasKey) {
+      if (value1 === value2) {
+        acc[`${key}`] = value1;
+        return acc;
+      }
+      acc[`- ${key}`] = value1;
+      acc[`+ ${key}`] = value2;
+      return acc;
+    } if (obj1HasKey && !obj2HasKey) {
+      acc[`- ${key}`] = value1;
+      return acc;
     }
-    return resultMass.reduce(cb, {});
+    acc[`+ ${key}`] = value2;
+    return acc;
+  };
+  return resultMass.reduce(cb, {});
 };
