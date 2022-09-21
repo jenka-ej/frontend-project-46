03a2c79e3c075e1bbacfcac55cc0bcd17328ccde
Evgeny Filippov <85373955+jenka-ej@users.noreply.github.com>
2022-09-21 15:51:03 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
 import fs from 'fs';
 import path from 'path';
 import compareData from './compareData.js';
-import goodView from './formatters/stylish.js';
+import getFormat from './formatters/index.js';
 import parser from './parser.js';
 
 const getExtension = (filepath) => path.extname(filepath);
 const getAbsolutePath = (filepath) => path.resolve(filepath);
 const readFileSync = (filepath) => fs.readFileSync(filepath, 'utf-8');
 
-const genDiff = (fpath1, fpath2) => {
+const genDiff = (fpath1, fpath2, formatName = 'stylish') => {
   const ext1 = getExtension(fpath1);
   const ext2 = getExtension(fpath2);
 
   const parsedFile1 = parser(readFileSync(getAbsolutePath(fpath1)), ext1);
   const parsedFile2 = parser(readFileSync(getAbsolutePath(fpath2)), ext2);
 
-  return goodView(compareData(parsedFile1, parsedFile2));
+  const differences = compareData((parsedFile1, parsedFile2), formatName);
+  return differences;
 };
 
 export default genDiff;
