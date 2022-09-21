64197d08145b50c29348ce39e4870328651419ea
Evgeny Filippov <85373955+jenka-ej@users.noreply.github.com>
2022-09-21 16:03:22 +0300

    Update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,7 +10,7 @@ program
 program
   .argument('<filepath1>')
   .argument('<filepath2>')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .action((filepath1, filepath2, options) => {
     console.log(genDiff(filepath1, filepath2, options.format));
   });
