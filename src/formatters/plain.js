7352a9eb705692d98b63090704854a0ae003ba2e
Evgeny Filippov <85373955+jenka-ej@users.noreply.github.com>
2022-09-25 15:28:08 +0300

    Update plain.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -72,32 +72,34 @@ const plain = (objDiff) => {
       return `Property '${key}' was added with value: '${value}'`;
     }
     const [sign, key, value1, value2] = massive;
-    const valueS1 = (value1 === true
-                    || value1 === false
-                    || value1 === null
-                    || value1 === '[complex value]');
-    const valueS2 = (value2 === true
-                    || value2 === false
-                    || value2 === null
-                    || value2 === '[complex value]');
-    if (value1 === '') {
-      if (valueS2) {
-        return `Property '${key}' was updated. From '' to ${value2}`;
+    if (sign === '-+') {
+      const valueS1 = (value1 === true
+                      || value1 === false
+                      || value1 === null
+                      || value1 === '[complex value]');
+      const valueS2 = (value2 === true
+                      || value2 === false
+                      || value2 === null
+                      || value2 === '[complex value]');
+      if (value1 === '') {
+        if (valueS2) {
+          return `Property '${key}' was updated. From '' to ${value2}`;
+        }
+        return `Property '${key}' was updated. From '' to '${value2}'`;
+      } if (value2 === '') {
+        if (valueS1) {
+          return `Property '${key}' was updated. From ${value1} to ''`;
+        }
+        return `Property '${key}' was updated. From '${value1}' to ''`;
+      } if (valueS1 && valueS2) {
+        return `Property '${key}' was updated. From ${value1} to ${value2}`;
+      } if (valueS1 && !valueS2) {
+        return `Property '${key}' was updated. From ${value1} to '${value2}'`;
+      } if (!valueS1 && valueS2) {
+        return `Property '${key}' was updated. From '${value1}' to ${value2}`;
       }
-      return `Property '${key}' was updated. From '' to '${value2}'`;
-    } if (value2 === '') {
-      if (valueS1) {
-        return `Property '${key}' was updated. From ${value1} to ''`;
-      }
-      return `Property '${key}' was updated. From '${value1}' to ''`;
-    } if (valueS1 && valueS2) {
-      return `Property '${key}' was updated. From ${value1} to ${value2}`;
-    } if (valueS1 && !valueS2) {
-      return `Property '${key}' was updated. From ${value1} to '${value2}'`;
-    } if (!valueS1 && valueS2) {
-      return `Property '${key}' was updated. From '${value1}' to ${value2}`;
+      return `Property '${key}' was updated. From '${value1}' to '${value2}'`;
     }
-    return `Property '${key}' was updated. From '${value1}' to '${value2}'`;
   });
   const plainResultText = JSON.stringify(text(finalResult)).split('"').join('')
     .split(',')
