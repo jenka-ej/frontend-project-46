d7a252c7abdd944a20f02fc6543980332183ff66
Evgeny Filippov <85373955+jenka-ej@users.noreply.github.com>
2022-09-26 14:47:20 +0300

    Update compareData.js

--- src/compareData.js
+++ src/compareData.js
@@ -1,48 +1,74 @@
 const compareData = (obj1, obj2) => {
-  const getChildren = (obj) => Object.keys(obj);
-  const childrenObj1 = getChildren(obj1);
-  const childrenObj2 = getChildren(obj2);
-  const childrenMass = childrenObj1.concat(childrenObj2);
-  const final = childrenMass.filter((child, index) => childrenMass.indexOf(child) === index).sort();
-  const cb = (acc, key) => {
-    const obj1HasKey = Object.getOwnPropertyDescriptor(obj1, key);
-    const obj2HasKey = Object.getOwnPropertyDescriptor(obj2, key);
-    const value1 = obj1[key];
-    const value2 = obj2[key];
-    const obj1KeyIsObject = (typeof value1 === 'object' && value1 !== null && !Array.isArray(value1));
-    const obj2KeyIsObject = (typeof value2 === 'object' && value2 !== null && !Array.isArray(value2));
-    if (obj1HasKey && obj2HasKey && obj1KeyIsObject && obj2KeyIsObject) {
-      acc[`${key}`] = compareData(value1, value2);
-      return acc;
-    } if (obj1HasKey && obj2HasKey && obj1KeyIsObject && !obj2KeyIsObject) {
-      acc[`- ${key}`] = value1;
-      acc[`+ ${key}`] = value2;
-      return acc;
-    } if (obj1HasKey && obj2HasKey && !obj1KeyIsObject && obj2KeyIsObject) {
-      acc[`- ${key}`] = value1;
-      acc[`+ ${key}`] = value2;
-      return acc;
-    } if (obj1HasKey && !obj2HasKey && obj1KeyIsObject) {
-      acc[`- ${key}`] = value1;
-      return acc;
-    } if (!obj1HasKey && obj2HasKey && obj2KeyIsObject) {
-      acc[`+ ${key}`] = value2;
-      return acc;
-    } if (obj1HasKey && obj2HasKey) {
-      if (value1 === value2) {
-        acc[`${key}`] = value1;
-        return acc;
-      }
-      acc[`- ${key}`] = value1;
-      acc[`+ ${key}`] = value2;
-      return acc;
-    } if (obj1HasKey && !obj2HasKey) {
-      acc[`- ${key}`] = value1;
-      return acc;
-    }
-    acc[`+ ${key}`] = value2;
-    return acc;
-  };
-  return final.reduce(cb, {});
+    const getChildren = (obj) => Object.keys(obj);
+    const childrenObj1 = getChildren(obj1);
+    const childrenObj2 = getChildren(obj2);
+    const childrenMass = childrenObj1.concat(childrenObj2);
+    const final = childrenMass.filter((child, index) => childrenMass.indexOf(child) === index).sort();
+    return final.map((key) => {
+        const obj1HasKey = Object.getOwnPropertyDescriptor(obj1, key);
+        const obj2HasKey = Object.getOwnPropertyDescriptor(obj2, key);
+        const value1 = obj1[key];
+        const value2 = obj2[key];
+        const obj1KeyIsObject = (typeof value1 === 'object' && value1 !== null && !Array.isArray(value1));
+        const obj2KeyIsObject = (typeof value2 === 'object' && value2 !== null && !Array.isArray(value2));
+        if (obj1HasKey && obj2HasKey && obj1KeyIsObject && obj2KeyIsObject) {
+            return {
+                'type': 'parent',
+                key,
+                'children': diff(value1, value2)
+            };
+        } if (obj1HasKey && obj2HasKey && obj1KeyIsObject && !obj2KeyIsObject) {
+            return {
+                'type': 'diffValue',
+                key,
+                'children': value1,
+                'children2': value2
+            };
+        } if (obj1HasKey && obj2HasKey && !obj1KeyIsObject && obj2KeyIsObject) {
+            return {
+                'type': 'diffValue',
+                key,
+                'children': value1,
+                'children2': value2
+            };
+        } if (obj1HasKey && !obj2HasKey && obj1KeyIsObject) {
+            return {
+                'type': 'deleted',
+                key,
+                'children': value1
+            };
+        } if (!obj1HasKey && obj2HasKey && obj2KeyIsObject) {
+            return {
+                'type': 'added',
+                key,
+                'children': value2
+            };
+        } if (obj1HasKey && obj2HasKey) {
+            if (value1 === value2) {
+                return {
+                    'type': 'stay same',
+                    key,
+                    'children': value1
+                };
+            }
+            return {
+                'type': 'diffValue',
+                key,
+                'children': value1,
+                'children2': value2
+            };
+        } if (obj1HasKey && !obj2HasKey) {
+            return {
+                'type': 'deleted',
+                key,
+                'children': value1
+            };
+        }
+        return {
+            'type': 'added',
+            key,
+            'children': value2
+        };
+    });
 };
 export default compareData;
