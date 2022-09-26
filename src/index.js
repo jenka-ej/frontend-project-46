263421a889f84e2c5c5884c2558252d85a423e3d
Evgeniy <Zhekafilipp@mail.ru>
2022-09-26 11:24:11 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const getPath = (way) => path.resolve(process.cwd(), way);
 const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
   const absolutePath1 = getPath(filepath1);
   const absolutePath2 = getPath(filepath2);
-  
+
   const content1 = fs.readFileSync(absolutePath1, 'utf-8');
   const content2 = fs.readFileSync(absolutePath2, 'utf-8');
 
